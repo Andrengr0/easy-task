1300cd4d7e98c5536960a93da71453c9aaa7f44f
André Negreiros <112659872+Andrengr0@users.noreply.github.com>
2024-06-26 00:15:56 -0300

    feat: Adição de janela de confirmação na remoção de tarefa

--- frontend/src/components/Todo.jsx
+++ frontend/src/components/Todo.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import '../App.css'
-import styles from './Todo.module.css'
+import React, { useState } from 'react';
+import '../App.css';
+import styles from './Todo.module.css';
 
 const getCategoryColor = (category) => {
   switch (category) {
@@ -15,24 +15,60 @@ const getCategoryColor = (category) => {
   }
 };
 
-const Todo = ({todo, removeTodo, completeTodo}) => {
+const Todo = ({ todo, removeTodo, completeTodo }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const openModal = () => {
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+  };
+
+  const handleRemove = () => {
+    removeTodo(todo.id);
+    closeModal(); // Fecha a modal após remover a tarefa
+  };
+
   return (
-    <div className={styles.todo} style={{textDecoration: todo.isCompleted ? "line-through" : ""}}>
-        <div className="content">
-            <p>{todo.text}</p>
-            <p className="category" style={{ color: getCategoryColor(todo.category) }}>{todo.category}</p>
-        </div>
-        <div>
+    <div className={styles.todo} style={{ textDecoration: todo.isCompleted ? 'line-through' : '' }}>
+      <div className="content">
+        <p>{todo.text}</p>
+        <p className="category" style={{ color: getCategoryColor(todo.category) }}>
+          {todo.category}
+        </p>
+      </div>
+      <div>
         <button
           className={`${styles.complete} ${todo.isCompleted ? styles.undo : ''}`}
           onClick={() => completeTodo(todo.id)}
         >
           {todo.isCompleted ? 'Desfazer' : 'Completar'}
         </button>
-            <button className={styles.remove} onClick={()=> removeTodo(todo.id)}>X</button>
+        <button className={styles.remove} onClick={openModal}>
+          X
+        </button>
+      </div>
+
+      {/* Modal de Confirmação */}
+      {showModal && (
+        <div className={styles.modal}>
+          <div className={styles.modalContent}>
+            <p>Deseja realmente remover esta tarefa?</p>
+            <div className={styles.modalButtons}>
+              <button className={styles.cancelButton} onClick={closeModal}>
+                Cancelar
+              </button>
+              <button className={styles.confirmButton} onClick={handleRemove}>
+                Confirmar
+              </button>
+            </div>
+          </div>
         </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 export default Todo;
